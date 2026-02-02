2ee20ab977cd83211874bbacc0421cdb3d9977ca
Tejasri Mokurala <165499725+Tejasrimokurala@users.noreply.github.com>
2026-02-02 23:37:28 +0530

    Add files via upload

---
+++ Arrays/MultipleArrays.java
@@ -0,0 +1,17 @@
+class MultipleArrays {
+    public static void main(String[] args) {
+
+        int a[] = {1, 2, 3};
+        int b[] = {4, 5, 6};
+
+        System.out.println("First array:");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+        System.out.println("Second array:");
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
+    }
+}
