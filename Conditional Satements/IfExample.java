80fd482f91aa3c2f463170f0d52e9c0f779c14b5
Tejasri Mokurala <165499725+Tejasrimokurala@users.noreply.github.com>
2026-02-05 23:35:26 +0530

    Add files via upload

---
+++ Conditional Satements/IfExample.java
@@ -0,0 +1,9 @@
+public class IfExample {
+    public static void main(String[] args) {
+        int age = 18;
+
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        }
+    }
+}
